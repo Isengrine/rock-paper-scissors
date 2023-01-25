61ca3a31c61b01d822b33dd5fe1225c020e43205
Isengrine <isengrine@gmail.com>
2023-01-24 23:15:24 -0600

    Add game function to get a winner out of 5 games

--- script.js
+++ script.js
@@ -5,31 +5,33 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerChoice) {
     if (playerSelection === computerChoice) {
-        return console.log("It's a tie!")
+        playerWin = true 
+        playerLose = true
+        return
     }
 
     else if (playerSelection == 0 && computerChoice == 1) {
-        return console.log("Paper beats Rock. You lose!")
+        return playerLose = true
     }
 
     else if (playerSelection == 0 && computerChoice == 2) {
-        return console.log("Rock beats Scissors. You win!")
+        return playerWin = true
     }
 
     else if (playerSelection == 1 && computerChoice == 2) {
-        return console.log("Scissors beats Paper. You lose!")
+        return playerLose = true
     }
 
     else if (playerSelection == 1 && computerChoice == 0) {
-        return console.log("Paper beats Rock. You win!")
+        return playerWin = true
     }
 
     else if (playerSelection == 2 && computerChoice == 0) {
-        return console.log("Rock beats Scissors. You lose!")
+        return playerLose = true
     }
 
     else if (playerSelection == 2 && computerChoice == 1) {
-        return console.log("Scissors beats Paper. You win!")
+        return playerWin = true
     }
 
     else {
@@ -61,3 +63,56 @@ function playerChoice() {
     }
 }
 
+function game() {
+
+    var playerScore = 0
+    var computerScore = 0
+
+    while (playerScore < 5 && computerScore < 5) {
+        console.log("Player Score: ", playerScore)
+        console.log("Computer Score: ", computerScore)
+        playRound(playerChoice(), getComputerChoice())
+
+        if (playerWin == true && playerLose == true) {
+            playerScore += 1
+            computerScore += 1
+            playerWin = false
+            playerLose = false
+        }
+
+        else if (playerWin == true) {
+            playerScore += 1
+            playerWin = false
+        }
+
+        else if (playerLose == true) {
+            computerScore += 1
+            playerLose = false
+        }
+
+        else {
+            console.log("Something went horribly wrong...")
+        }
+    }
+
+    if (playerScore == computerScore) {
+        console.log("It's a tie!")
+    }
+
+    else if (playerScore > computerScore) {
+        console.log("Player wins!")
+    }
+
+    else if (playerScore < computerScore) {
+        console.log("Computer wins!")
+    }
+
+    else {
+        console.log("Something went horribly wrong...")
+    }
+}
+
+var playerWin = false
+var playerLose = false
+
+game()
