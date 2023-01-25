a2ed6bc33a59135c7d0c0caba69fc709e1982ccf
Isengrine <isengrine@gmail.com>
2023-01-24 22:19:41 -0600

    Add play round function

--- script.js
+++ script.js
@@ -1,10 +1,40 @@
 function getComputerChoice() {
     computerChoice = Math.floor(Math.random() * 3)
-    return console.log(computerChoice)
+    return computerChoice
 }
 
 function playRound(playerSelection, computerChoice) {
+    if (playerSelection === computerChoice) {
+        return console.log("It's a tie!")
+    }
 
+    else if (playerSelection == 0 && computerChoice == 1) {
+        return console.log("Paper beats Rock. You lose!")
+    }
+
+    else if (playerSelection == 0 && computerChoice == 2) {
+        return console.log("Rock beats Scissors. You win!")
+    }
+
+    else if (playerSelection == 1 && computerChoice == 2) {
+        return console.log("Scissors beats Paper. You lose!")
+    }
+
+    else if (playerSelection == 1 && computerChoice == 0) {
+        return console.log("Paper beats Rock. You win!")
+    }
+
+    else if (playerSelection == 2 && computerChoice == 0) {
+        return console.log("Rock beats Scissors. You lose!")
+    }
+
+    else if (playerSelection == 2 && computerChoice == 1) {
+        return console.log("Scissors beats Paper. You win!")
+    }
+
+    else {
+        return console.log("Something went horribly wrong...")
+    }
 }
 
 function playerChoice() {
@@ -12,17 +42,17 @@ function playerChoice() {
 
     if (choice == "rock") {
         choice = 0
-        return console.log(choice)
+        return choice
     }
 
     else if (choice == "paper") {
         choice = 1
-        return console.log(choice)
+        return choice
     }
 
     else if (choice == "scissors") {
         choice = 2
-        return console.log(choice)
+        return choice
     }
 
     else {
@@ -30,3 +60,4 @@ function playerChoice() {
         return playerChoice()
     }
 }
+
