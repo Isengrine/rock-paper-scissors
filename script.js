5193b5182fc8a72cb3ff5bf6069623c7c8d01aef
Isengrine <isengrine@gmail.com>
2023-01-24 21:26:05 -0600

    Add player choice function

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("test")
+function getComputerChoice() {
+    computerChoice = Math.floor(Math.random() * 3)
+    return console.log(computerChoice)
+}
+
+function playRound(playerSelection, computerChoice) {
+
+}
+
+function playerChoice() {
+    choice = prompt("Choose either Rock, Paper or Scissors").toLowerCase()
+
+    if (choice == "rock") {
+        choice = 0
+        return console.log(choice)
+    }
+
+    else if (choice == "paper") {
+        choice = 1
+        return console.log(choice)
+    }
+
+    else if (choice == "scissors") {
+        choice = 2
+        return console.log(choice)
+    }
+
+    else {
+        alert("You have to choose either Rock, Paper or Scissors")
+        return playerChoice()
+    }
+}
