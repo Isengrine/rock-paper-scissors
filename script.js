f2f120b0d962c7528cb1eaee5f1da93b6ab8b256
Isengrine <isengrine@gmail.com>
2023-01-26 07:00:50 -0600

    Add final score function

--- script.js
+++ script.js
@@ -4,10 +4,25 @@ function getComputerChoice() {
     return computerChoice;
 }
 
+// Generates a prompt to ask for a player choice and turns it into an number from 0 to 2. (0=rock, 1=paper, 2=scissors)
+function playerChoice(choice) {
+    if (choice == "rock") {
+        return choice = 0;
+    }
+
+    else if (choice == "paper") {
+        return choice = 1;
+    }
+
+    else if (choice == "scissors") {
+        return choice = 2;
+    }
+}
+
 // Compares playerSelection and computerChoice and declares a winner (or a tie)
 function playRound(playerSelection, computerChoice) {
     if (playerSelection === computerChoice) {
-        playerWin = true ;
+        playerWin = true;
         computerWin = true;
         console.log("It's a tie!");
         return;
@@ -50,23 +65,8 @@ function playRound(playerSelection, computerChoice) {
     }
 }
 
-// Generates a prompt to ask for a player choice and turns it into an number from 0 to 2. (0=rock, 1=paper, 2=scissors)
-function playerChoice(choice) {
-    if (choice == "rock") {
-        return choice = 0;
-    }
-
-    else if (choice == "paper") {
-        return choice = 1;
-    }
-
-    else if (choice == "scissors") {
-        return choice = 2;
-    }
-}
-
 // Checks the score of the player vs the computer in the main game function and declares a winner.
-function checkScore(playerScore, computerScore) {
+function checkFinalScore(playerScore, computerScore) {
     if (playerScore == computerScore) {
         console.log("It's a tie!");
     }
@@ -82,36 +82,41 @@ function checkScore(playerScore, computerScore) {
 
 // Main game function. Runs the game in a while loop until a score reaches 5, then runs checkScore function.
 function game() {
-
-    let playerScore = 0;
-    let computerScore = 0;
-
     while (playerScore < 5 && computerScore < 5) {
-        console.log("Player Score: ", playerScore);
-        console.log("Computer Score: ", computerScore);
-        playRound(playerChoice(), getComputerChoice());
-
         if (playerWin == true && computerWin == true) {
-            playerScore += 1;
-            computerScore += 1;
             playerWin = false;
             computerWin = false;
+            console.log("Player Score: ", playerScore);
+            console.log("Computer Score: ", computerScore);
+            break;
         }
 
         else if (playerWin == true) {
             playerScore += 1;
             playerWin = false;
+            console.log("Player Score: ", playerScore);
+            console.log("Computer Score: ", computerScore);
+            break;
         }
 
         else if (computerWin == true) {
             computerScore += 1;
             computerWin = false;
+            console.log("Player Score: ", playerScore);
+            console.log("Computer Score: ", computerScore);
+            break;
         }
     }
 
-    checkScore(playerScore, computerScore);
-
+    if (playerScore >= 5 || computerScore >= 5) {
+        checkFinalScore(playerScore, computerScore);
+    }
 }
+
+let playerWin = false;
+let computerWin = false;
+let playerScore = 0;
+let computerScore = 0;
 
 const rck = document.querySelector("#rck");
 const ppr = document.querySelector("#ppr");
@@ -120,17 +125,17 @@ const scs = document.querySelector("#scs");
 rck.addEventListener("click", () => {
     let choice = playerChoice("rock");
     let computerChoice = getComputerChoice();
-    playRound(choice,computerChoice)});
+    playRound(choice,computerChoice)
+    game()});
 
 ppr.addEventListener("click", () => {
     let choice = playerChoice("paper");
     let computerChoice = getComputerChoice();
-    playRound(choice,computerChoice)});
+    playRound(choice,computerChoice);
+    game()});
     
 scs.addEventListener("click", () => {
     let choice = playerChoice("scissors");
     let computerChoice = getComputerChoice();
-    playRound(choice,computerChoice)});
-
-let playerWin = false;
-let computerWin = false;
+    playRound(choice,computerChoice);
+    game()});
